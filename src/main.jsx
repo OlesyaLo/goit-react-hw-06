1b57e8e592d199d1f95baac38e27afe3163a2a73
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-10-08 01:30:08 +0200

    added basic settings

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App.jsx';
+// 1. Імпортуємо провайдер
+import { Provider } from 'react-redux'
+// 2. Імпортуємо створений раніше стор
+import { store } from "./redux/store.js";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+	  <Provider store={store}>
+	    <App />
+	  </Provider>
+  </React.StrictMode>
+);
